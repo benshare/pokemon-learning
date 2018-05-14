9881933a7f2e975bc26c26e3b516fec11deefac6
zack5 <icblueskye@gmail.com>
2018-05-13 21:26:03 -0700

    Consolodated readGif into PokmeonUtils; better documentation / structure for PokemonUtils; added permanent Test/Val/Train splits, accessed through PokemonUtils

---
+++ Utils/MakeTrainDevTestSplits.py
@@ -0,0 +1,35 @@
+from PokemonUtils import *
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    save_folder = "../Data/Splits/"
+    train_percent, dev_percent = 0.64, 0.80;  # 80/20 (train+val)/test, 80/20 train/val
+
+    utils = PokemonUtils()
+    n = utils.PrimaryTypesArray.shape[0]
+    inds = np.arange(n)
+    np.random.shuffle(inds)
+    train_inds = inds[:int(n*train_percent)]
+    val_inds = inds[int(n*train_percent):int(n*dev_percent)]
+    test_inds = inds[int(n*dev_percent):]
+
+    train, val, test = (
+        utils.PrimaryTypesArray[train_inds,:],
+        utils.PrimaryTypesArray[val_inds,:],
+        utils.PrimaryTypesArray[test_inds,:])
+
+    print("train: " + str(train.shape[0]))
+    print("dev:   " + str(val.shape[0]))
+    print("test:  " + str(test.shape[0]))
+
+    split_names = ("Train", "Val", "Test")
+    splits = (train, val, test)
+    for i in range(3):
+        fig, ax = plt.subplots()
+        ax.hist(splits[i][:,1], np.arange(20), rwidth=0.75)
+        ax.set_xticks(np.arange(18)+1.5)
+        ax.set_xticklabels(utils.type_names, rotation=45, rotation_mode="anchor", ha="right")
+        plt.title = split_names[i] + " Distribution"
+        plt.savefig(save_folder + split_names[i] + "Distribution.png")
+
+        np.savetxt(save_folder + split_names[i] + '.txt', splits[i], fmt='%i')
--- Utils/PokemonUtils.py
+++ Utils/PokemonUtils.py
@@ -1,64 +1,70 @@
 import csv
 import numpy as np
-import os.path
+import matplotlib.pyplot as plt
+import os.path  # File path checking
+from PIL import Image, ImageSequence  # Reading GIFs
+
+
+"""
+PokemonUtils
+------------
+* Useage: utils = PokemonUtils()
+
+
+DATA:
+-----
+* utils.x_train, .x_val, .x_test
+(N x 1) arrays of pokedex numbers
+
+* .y_train, .y_val, .y_test
+(N x 1) arrays of type numbers (1 = Normal, ..., 18 = Fairy)
+
+
+GIF MANIPULATION:
+-----------------
+* utils.readGif(path)
+gif path name --> numpy array (T x H x W x C)
+
+
+LOOKUP TABLES:
+--------------
+* utils.typeToNameDict, .nameToTypeDict
+* utils.typeToName(n)
+Dictionaries of type number <-> type name
+
+* utils.nameToNumberDict, .NumberToNameDict
+* utils.nameToNumber(name), .numberToName(n)
+Dictionaries of pokedex number <-> pokemon name
+
+
+OTHER:
+------
+* utils.uniqueValidIDs
+List of pokedex numbers of all pokemon we will be using
+
+* utils.uniqueValidPokemonNames
+List of names of all pokemon we will be using
+
+"""
 
 class PokemonUtils():
 
     def __init__(self, verbose=True):
-        self.path = "pokedex-master/pokedex/data/csv/"
-        self.gifsPath = "pkparaiso"
+        self.csv_path = "../Data/veekun/"
+        self.gifs_path = "../Data/pkparaiso/"
+        self.splits_path = "../Data/Splits/"
         self.verbose = verbose
         self.generateNameToNumber()  # loads all names/numbers from veekun file
-        self.generateMissingImages(self.gifsPath)  # cross-references with downloaded images
+        self.generateMissingImages(self.gifs_path)  # cross-references with downloaded images
         self.generateNumberToTypes()  # Only for pokemon that have associated images
         self.generateTypeNames()  # Only for pokemon that have associated images
-        self.generateTrainValTestSplit(0.8, 0.8)
-
-    def normalizeString(self, s):
-        return s.lower()
-
-    def generateTypeNames(self):
-        self.typeToNameDict = {}
-        self.nameToTypeDict = {}
-        with open(self.path + "type_names.csv") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                name = self.normalizeString(row['name'])
-                number = int(row['type_id'])
-                if row['local_language_id'] == '9':
-                    self.typeToNameDict[number] = name
-                    self.nameToTypeDict[name] = number
-
-    def generateNumberToTypes(self):
-        self.numberToTypesDict = {}
-        self.numberToPrimaryTypeDict = {}
-        self.numberToSecondaryTypeDict = {}
-        self.uniqueValidIDs = []
-        with open(self.path + "pokemon_types.csv") as f:
-            reader = csv.DictReader(f)
-            for row in reader:
-                pokemon_id = int(row['pokemon_id'])
-                type_id = int(row['type_id'])
-                if pokemon_id in self.missingImageNumbers:
-                    continue  # ignore pokemon that we don't have images for
-                if row['slot'] == '1':
-                    self.uniqueValidIDs.append(pokemon_id)
-                    self.numberToTypesDict[pokemon_id] = [type_id]
-                    self.numberToPrimaryTypeDict[pokemon_id] = type_id
-                    self.numberToSecondaryTypeDict[pokemon_id] = -1
-                else:
-                    self.numberToTypesDict[pokemon_id].append(type_id)
-                    self.numberToSecondaryTypeDict[pokemon_id] = type_id
-
-        self.uniqueValidIDsArray = np.array(self.uniqueValidIDs)
-        self.PrimaryTypesArray   = np.array([[n, self.numberToPrimaryTypeDict[n]] for n in self.uniqueValidIDs])
-        self.SecondaryTypesArray = np.array([[n, self.numberToSecondaryTypeDict[n]] for n in self.uniqueValidIDs])
+        self.generateTrainValTestSplit()  # Load from file
 
     def generateNameToNumber(self):
         self.nameToNumberDict = {}
         self.numberToNameDict = {}
         self.uniqueIDs = []
-        with open(self.path + "pokemon.csv") as f:
+        with open(self.csv_path + "pokemon.csv") as f:
             reader = csv.DictReader(f)
             for row in reader:
                 id = int(row['id'])
@@ -69,15 +75,76 @@ class PokemonUtils():
 
         self.allNames = self.numbersToName(self.uniqueIDs)
 
-    def generateTrainValTestSplit(self, train, dev):
-        n = self.PrimaryTypesArray.shape[0]
-        inds = np.arange(n)
-        np.random.shuffle(inds)
-        train_inds = inds[:int(n*train)]
-        val_inds = inds[int(n*train):int(n*dev)]
-        test_inds = inds[int(n*dev):]
-        self.x_train, self.x_val, self.x_test = self.PrimaryTypesArray[train_inds,0], self.PrimaryTypesArray[val_inds,0], self.PrimaryTypesArray[test_inds,0]
-        self.y_train, self.y_val, self.y_test = self.PrimaryTypesArray[train_inds,1], self.PrimaryTypesArray[val_inds,1], self.PrimaryTypesArray[test_inds,1]
+    def generateMissingImages(self, folder):
+        self.missingImageNames = []
+        self.missingImageNumbers = []
+        for name in self.allNames:
+            if not os.path.isfile(folder + "/" + name + ".gif"):
+                self.missingImageNames.append(name)
+                self.missingImageNumbers.append(self.nameToNumber(name))
+        if len(self.missingImageNames) > 0:
+            print("Warning: can't find", len(self.missingImageNames), "images!")
+
+    def generateNumberToTypes(self):
+        self.numberToTypesDict = {}
+        self.numberToPrimaryTypeDict = {}
+        self.numberToSecondaryTypeDict = {}
+        self.uniqueValidIDs = []
+        self.uniqueValidPokemonNames = []
+        with open(self.csv_path + "pokemon_types.csv") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                pokemon_id = int(row['pokemon_id'])
+                type_id = int(row['type_id'])
+                if pokemon_id in self.missingImageNumbers:
+                    continue  # ignore pokemon that we don't have images for
+                if row['slot'] == '1':
+                    self.uniqueValidIDs.append(pokemon_id)
+                    self.uniqueValidPokemonNames.append(self.numberToName(pokemon_id))
+                    self.numberToTypesDict[pokemon_id] = [type_id]
+                    self.numberToPrimaryTypeDict[pokemon_id] = type_id
+                    self.numberToSecondaryTypeDict[pokemon_id] = -1
+                else:
+                    self.numberToTypesDict[pokemon_id].append(type_id)
+                    self.numberToSecondaryTypeDict[pokemon_id] = type_id
+
+        # By now, we know which pokemon we will use
+        self.uniqueValidIDsArray = np.array(self.uniqueValidIDs)
+        self.PrimaryTypesArray   = np.array([[n, self.numberToPrimaryTypeDict[n]] for n in self.uniqueValidIDs])
+        self.SecondaryTypesArray = np.array([[n, self.numberToSecondaryTypeDict[n]] for n in self.uniqueValidIDs])
+
+    def generateTypeNames(self):
+        self.type_names = []
+        self.typeToNameDict = {}
+        self.nameToTypeDict = {}
+        with open(self.csv_path + "type_names.csv") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                name = self.normalizeString(row['name'])
+                number = int(row['type_id'])
+                if row['local_language_id'] == '9':
+                    if number > 18:
+                        return
+                    self.typeToNameDict[number] = name
+                    self.nameToTypeDict[name] = number
+                    self.type_names.append(name)
+
+    def generateTrainValTestSplit(self):
+        try:
+            train = np.loadtxt(self.splits_path + "Train.txt", dtype=int)
+            val   = np.loadtxt(self.splits_path + "Val.txt", dtype=int)
+            test  = np.loadtxt(self.splits_path + "Test.txt", dtype=int)
+            self.x_train = train[:,0]
+            self.x_val   = val[:,0]
+            self.x_test  = test[:,0]
+            self.y_train = train[:,1]
+            self.y_val   = val[:,1]
+            self.y_test  = test[:,1]
+        except:
+            print("Error: couldn't load train/val/test splits")
+
+    def normalizeString(self, s):
+        return s.lower()
 
     def getTypeSample(self, type, k=5, split='all'):
         sample = None
@@ -114,7 +181,7 @@ class PokemonUtils():
             type_id = i+1
             html += "<h3>" + self.typeToName(type_id).capitalize() + "</h3>"
             for pkmn in self.numbersToName(self.getTypeSample(type_id, k, 'train')):
-                html += '<img src="' + self.gifsPath + "/" + pkmn + '.gif">'
+                html += '<img src="' + self.gifs_path + pkmn + '.gif">'
 
         optionTags = ""
         for i in range(18):
@@ -130,7 +197,7 @@ class PokemonUtils():
             y = self.y_test[index]
             pkmn = self.numberToName(x)
             html += "<div style='margin-bottom:15px' id='question" + str(i) + "'>"
-            html += '<img src="' + self.gifsPath + "/" + str(pkmn) + '.gif">'
+            html += '<img src="' + self.gifs_path + str(pkmn) + '.gif">'
             html += "<select>"
             html += optionTags
             html += "</select>"
@@ -170,19 +237,45 @@ class PokemonUtils():
     def nameToNumber(self, name):
         return self.nameToNumberDict[self.normalizeString(name)]
 
-    def generateMissingImages(self, folder):
-        self.missingImageNames = []
-        self.missingImageNumbers = []
-        for name in self.allNames:
-            if not os.path.isfile(folder + "/" + name + ".gif"):
-                self.missingImageNames.append(name)
-                self.missingImageNumbers.append(self.nameToNumber(name))
-        if len(self.missingImageNames) > 0:
-            print("Warning: can't find", len(self.missingImageNames), "images!")
+    # from https://stackoverflow.com/questions/50054187/convert-animated-gif-to-4d-array-in-python
+    # GIF path name --> 4D numpy array
+    def readGif(self, path, format='RGBA'):
+        channels = 4
+        if format != 'RGBA':
+            channels = 3
+
+        img = Image.open(path)
+        gif = np.array([np.array(frame.copy().convert(format).getdata(),dtype=np.uint8).reshape(frame.size[1],frame.size[0],channels) for frame in ImageSequence.Iterator(img)])
+        return gif
 
 if __name__ == "__main__":
     utils = PokemonUtils()
-    print(utils.numbersToName(utils.getTypeSample(utils.nameToType('flying'), 100, 'train')))
-    print(utils.numbersToName(utils.getTypeSample(utils.nameToType('flying'), 100, 'val')))
-    print(utils.numbersToName(utils.getTypeSample(utils.nameToType('flying'), 100, 'test')))
-    utils.generateTypeQuizHTML(k=10, q=20)
+
+    # Find distribution of data sizes
+    smallest = np.array([999,999,999,999])
+    largest  = np.array([-1,-1,-1,-1])
+    count = 1;
+    for name in utils.uniqueValidPokemonNames:
+        gif = utils.readGif("../Data/pkparaiso/" + name + ".gif")
+        cur = np.array([gif.shape[0], gif.shape[1], gif.shape[2], gif.shape[3]])
+        smallest = np.minimum(cur, smallest)
+        largest  = np.maximum(cur, largest)
+        if count % 50 == 0:
+            print(count, "done!")
+        count += 1
+    print("smallest:", smallest)
+    print("largest:", largest)
+
+    # Check readGif functionality
+    # gif = utils.readGif("../Data/pkparaiso/araquanid.gif")
+    # for i in range(3):
+    #     plt.imshow(gif[i,:,:,:])
+    #     plt.show()
+
+    # Check flying-type distribution
+    # print(utils.numbersToName(utils.getTypeSample(utils.nameToType('flying'), 100, 'train')))
+    # print(utils.numbersToName(utils.getTypeSample(utils.nameToType('flying'), 100, 'val')))
+    # print(utils.numbersToName(utils.getTypeSample(utils.nameToType('flying'), 100, 'test')))
+
+    # Generate Type Quiz
+    # utils.generateTypeQuizHTML(k=10, q=20)
